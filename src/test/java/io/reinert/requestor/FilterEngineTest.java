c59397687390b7c5e051497315bfaf94d27db43a
Danilo Reinert <daniloreinert@gmail.com>
2014-11-25 15:15:29 -0300

    FilterEngineTest.

---
+++ src/test/java/io/reinert/requestor/FilterEngineTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2014 Danilo Reinert
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.reinert.requestor;
+
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Unit tests of {@link FilterEngine}.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class FilterEngineTest {
+
+    @Mock private ResponseFilter responseFilter;
+    @Mock private RequestFilter requestFilter;
+    @Mock private FilterManager manager;
+
+    private FilterEngine engine;
+
+    @Before
+    public void initManager() {
+        when(manager.getRequestFilters()).thenReturn(Arrays.asList(requestFilter));
+        when(manager.getResponseFilters()).thenReturn(Arrays.asList(responseFilter));
+        engine = new FilterEngine(manager);
+    }
+
+    @Test
+    public void filterRequest_AnyRequestBuilder_ShouldApplyAllRegisteredFilters() {
+        // Given
+        RequestBuilder requestBuilder = mock(RequestBuilder.class);
+
+        // When
+        engine.filterRequest(requestBuilder);
+
+        // Then
+        verify(requestFilter).filter(requestBuilder);
+    }
+
+    @Test
+    public void filterResponse_AnyResponseBuilder_ShouldApplyAllRegisteredFilters() {
+        // Given
+        Request request = mock(Request.class);
+        Response response = mock(Response.class);
+
+        // When
+        engine.filterResponse(request, response);
+
+        // Then
+        verify(responseFilter).filter(request, response);
+    }
+}
