aa5144e08cbf997ae399adf59df0619c92a40d89
Danilo Reinert <daniloreinert@gmail.com>
2015-01-06 13:04:18 -0300

    generate-site script update.

--- generate-site.sh
+++ generate-site.sh
@@ -1,2 +1,7 @@
-mvn clean install -P!project,examples
-mvn site-deploy -P!project,site
+if ["$1" == "latest"]; then
+  mvn clean install -P!project,examples
+  mvn site-deploy -P!project,site -DsitePath=latest
+else
+  mvn clean install -P!project,examples
+  mvn site-deploy -P!project,site
+fi
