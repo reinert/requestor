0fd0fa217da599cd7b836186971bd4f9482b57d3
Danilo Reinert <daniloreinert@gmail.com>
2015-02-11 16:33:28 -0300

    Fixes in scripts.

--- release.sh
+++ release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # usage: sh release.sh 0.1.0 0.2.0-SNAPSHOT
+
 if [ -n "$1" ] && [ -n "$2" ]; then
-  mvn clean deploy
+  mvn clean deploy -P!project
   mvn install -P!project,examples
   mvn site-deploy -P!project,site
   mvn versions:set -DnewVersion=$1
@@ -22,4 +23,13 @@ if [ -n "$1" ] && [ -n "$2" ]; then
   git add .
   git commit -m "Start $2 development"
   git push origin master
+elif [ "snapshot" == "$1" ] || [ "current" == "$1" ]; then
+  sh ./generate-site.sh
+  mvn deploy:deploy -P!project
+else
+  echo "USAGE"
+  echo "1)  Release version: sh release.sh {release-version} {next-snapshot}"
+  echo "            Example: sh release.sh 0.1.0 0.2.0-SNAPSHOT"
+  echo "2) Release snapshot: sh release.sh snapshot"
+  echo "        Alternative: sh release.sh current"
 fi
