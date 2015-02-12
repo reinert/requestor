fd1dd0c54873925cd273cca7c53fe2c2400b32c6
Danilo Reinert <daniloreinert@gmail.com>
2015-02-12 16:37:29 -0300

    Header package refactoring.

--- requestor/core/requestor-api/src/main/java/io/reinert/requestor/header/AcceptHeader.java
+++ requestor/core/requestor-api/src/main/java/io/reinert/requestor/header/AcceptHeader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Danilo Reinert
+ * Copyright 2015 Danilo Reinert
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,11 +22,33 @@ package io.reinert.requestor.header;
  */
 public class AcceptHeader extends QualityFactorHeader {
 
-    public AcceptHeader(Value... values) {
-        super("Accept", values);
+    public AcceptHeader(String name, String v1, double f1, String v2, double f2, String v3, double f3,
+                        String v4, double f4, String v5, double f5) {
+        super("Accept", v1, f1, v2, f2, v3, f3, v4, f4, v5, f5);
     }
 
-    public AcceptHeader(String... values) {
+    public AcceptHeader(String name, String v1, double f1, String v2, double f2, String v3, double f3,
+                        String v4, double f4) {
+        super("Accept", v1, f1, v2, f2, v3, f3, v4, f4);
+    }
+
+    public AcceptHeader(String name, String v1, double f1, String v2, double f2, String v3, double f3) {
+        super("Accept", v1, f1, v2, f2, v3, f3);
+    }
+
+    public AcceptHeader(String name, String v1, double f1, String v2, double f2) {
+        super("Accept", v1, f1, v2, f2);
+    }
+
+    public AcceptHeader(String name, String v1, double f1) {
+        super("Accept", v1, f1);
+    }
+
+    public AcceptHeader(String name, String value) {
+        super("Accept", value);
+    }
+
+    public AcceptHeader(String name, String... values) {
         super("Accept", values);
     }
 }
--- requestor/core/requestor-api/src/main/java/io/reinert/requestor/header/MultivaluedHeader.java
+++ requestor/core/requestor-api/src/main/java/io/reinert/requestor/header/MultivaluedHeader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Danilo Reinert
+ * Copyright 2015 Danilo Reinert
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,61 +23,34 @@ package io.reinert.requestor.header;
 public class MultivaluedHeader extends Header {
 
     private final String name;
-    private final String value;
-    private final String[] values;
+    private final Value[] values;
+    private String valueString;
 
-    protected MultivaluedHeader(String name, Object... values) {
+    public MultivaluedHeader(String name, Value... values) {
         this.name = name;
-        this.values = new String[values.length];
-        for (int i = 0; i < values.length; i++) {
-            Object v = values[i];
-            this.values[i] = v.toString();
-        }
-        this.value = mountValue(this.values);
+        this.values = values;
     }
 
     public MultivaluedHeader(String name, String... values) {
         this.name = name;
-        this.values = values;
-        this.value = mountValue(values);
+        this.values = new Value[values.length];
+        for (int i = 0; i < values.length; i++) {
+            this.values[i] = Value.of(values[i]);
+        }
     }
 
-    /**
-     * Returns the name of the HTTP header.
-     *
-     * @return name of the HTTP header
-     */
     @Override
     public String getName() {
         return name;
     }
 
-    /**
-     * Returns the value of the HTTP header.
-     *
-     * @return value of the HTTP header
-     */
     @Override
     public String getValue() {
-        return value;
+        if (valueString == null) valueString = Value.toString(values);
+        return valueString;
     }
 
-    /**
-     * Returns the values of the HTTP header.
-     *
-     * @return values of the HTTP header
-     */
-    public String[] getValues() {
+    public Value[] getRawValues() {
         return values;
-    }
-
-    protected String mountValue(String[] values) {
-        String mountedValue = "";
-        String separator = "";
-        for (String v : values) {
-            mountedValue += separator + v;
-            separator = ", ";
-        }
-        return mountedValue;
     }
 }
