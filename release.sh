6144f3673304468d4ebc0177657165783e165284
Danilo Reinert <daniloreinert@gmail.com>
2015-02-19 00:07:35 -0200

    Release 0.2.0

--- release.sh
+++ release.sh
@@ -2,30 +2,35 @@
 # usage: sh release.sh 0.1.0 0.2.0-SNAPSHOT
 
 if [ -n "$1" ] && [ -n "$2" ]; then
-  mvn clean deploy -P!project
-  mvn install -P!project,examples
-  mvn site-deploy -P!project,site
+  # update version to release version
   mvn versions:set -DnewVersion=$1
   mvn versions:commit
+  # update tag to release version
   sh ./tag.sh $1
+  # commit
   git add .
   git commit -m "Release $1"
-  mvn clean install -Prelease
+  # deploy
+  mvn clean deploy -P!project,release
+  # tag
   git tag -a requestor-$1 -m "Requestor v$1"
   git push origin requestor-$1
-  mvn deploy:deploy -P!project
+  # site
   mvn install -P!project,examples
   mvn site-deploy -P!project,site
   mvn site-deploy -P!project,site -DsitePath=latest
+  # update version to next snapshot
   mvn versions:set -DnewVersion=$2
   mvn versions:commit
+  # update tag to next snapshot
   sh ./tag.sh HEAD
+  # commit
   git add .
   git commit -m "Start $2 development"
   git push origin master
 elif [ "snapshot" == "$1" ] || [ "current" == "$1" ]; then
   sh ./generate-site.sh
-  mvn deploy:deploy -P!project
+  mvn clean deploy -P!project
 else
   echo "USAGE"
   echo "1)  Release version: sh release.sh {release-version} {next-snapshot}"
