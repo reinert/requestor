4e148fe636ee1004a8e62d2fcb78b1c77f1b3f2d
Danilo Reinert <daniloreinert@gmail.com>
2015-02-25 15:07:53 -0300

    Renamed SerializedResponseImpl to RawResponseImpl.

--- requestor/core/requestor-api/src/main/java/io/reinert/requestor/RequestDispatcherImpl.java
+++ requestor/core/requestor-api/src/main/java/io/reinert/requestor/RequestDispatcherImpl.java
@@ -149,7 +149,7 @@ public class RequestDispatcherImpl extends RequestDispatcher {
                 final Payload payload = responseType.isEmpty() || responseType.equalsIgnoreCase("text") ?
                         new Payload(xhr.getResponseText()) : new Payload(xhr.getResponse());
 
-                final SerializedResponseImpl response = new SerializedResponseImpl(gwtResponse.getStatusCode(),
+                final RawResponseImpl response = new RawResponseImpl(gwtResponse.getStatusCode(),
                         gwtResponse.getStatusText(), new Headers(gwtResponse.getHeaders()),
                         ResponseType.of(responseType), payload);
 
