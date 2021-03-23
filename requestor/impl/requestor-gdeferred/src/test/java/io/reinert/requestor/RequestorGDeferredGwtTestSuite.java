87fb0f2da2aeb5bcc87d7a21517a81c02c221642
Danilo Reinert <daniloreinert@gmail.com>
2021-03-23 09:43:02 -0300

    Added tests to requestor-gdeferred callbacks

--- requestor/impl/requestor-gdeferred/src/test/java/io/reinert/requestor/RequestorGDeferredGwtTestSuite.java
+++ requestor/impl/requestor-gdeferred/src/test/java/io/reinert/requestor/RequestorGDeferredGwtTestSuite.java
@@ -25,6 +25,9 @@ public class RequestorGDeferredGwtTestSuite extends GWTTestSuite {
     public static Test suite() {
         TestSuite suite = new TestSuite("Requestor GDeferred GWT Test Suite");
 
+        // Callbacks
+        suite.addTestSuite(CallbackGwtTest.class);
+
         // Serialization
         suite.addTestSuite(SpecialTypeResponsesGwtTest.class);
 
