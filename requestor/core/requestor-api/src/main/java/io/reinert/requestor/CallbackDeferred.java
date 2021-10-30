48fe0ed7cbeea32f7b9d90f58866d6aa2595dd0e
Danilo Reinert <daniloreinert@gmail.com>
2021-10-30 16:42:02 -0300

    Fix AbstractService copyright header

--- requestor/core/requestor-api/src/main/java/io/reinert/requestor/CallbackDeferred.java
+++ requestor/core/requestor-api/src/main/java/io/reinert/requestor/CallbackDeferred.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.reinert.requestor;
 
 import com.google.gwt.core.client.Callback;
