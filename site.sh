38e258d7792ccad849bae134e53c76c0c66ca3c3
Danilo Reinert <daniloreinert@gmail.com>
2021-12-03 17:02:27 -0300

    Update site.sh

--- site.sh
+++ site.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rm -rf $1/examples/showcase                                                                                 
+rm -rf $1
+cp -r ../requestor/target/site $1/
+mkdir $1/examples
 cp -r ../requestor/examples/requestor-showcase/src/main/webapp $1/examples/showcase/
 cp -r ../requestor/examples/requestor-showcase/target/requestor-showcase-$1/showcase $1/examples/showcase
 cp -r ../requestor/examples/requestor-showcase/target/requestor-showcase-$1/WEB-INF $1/examples/showcase
