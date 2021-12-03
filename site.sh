2a1e92f6751d9e0d251c6eb6aa1153380a3f6948
Danilo Reinert <daniloreinert@gmail.com>
2021-12-03 10:56:44 -0300

    Building site for Requestor 0.3.0-SNAPSHOT

--- site.sh
+++ site.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -rf $1/examples/showcase/showcase                                                                                 
-rm -rf $1/examples/showcase/WEB-INF 
+rm -rf $1/examples/showcase                                                                                 
+cp -r ../requestor/examples/requestor-showcase/src/main/webapp $1/examples/showcase/
 cp -r ../requestor/examples/requestor-showcase/target/requestor-showcase-$1/showcase $1/examples/showcase
 cp -r ../requestor/examples/requestor-showcase/target/requestor-showcase-$1/WEB-INF $1/examples/showcase
 git add .
