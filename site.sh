7a5900a41c2e35a52680d9a908b28187c213363e
Danilo Reinert <daniloreinert@gmail.com>
2022-03-14 12:29:21 -0300

    Update site.sh

--- site.sh
+++ site.sh
@@ -2,10 +2,10 @@
 
 rm -rf $1
 cp -r ../requestor/target/site $1/
-mkdir $1/examples
-cp -r ../requestor/examples/requestor-showcase/src/main/webapp $1/examples/showcase/
-cp -r ../requestor/examples/requestor-showcase/target/requestor-showcase-$1/showcase $1/examples/showcase
-cp -r ../requestor/examples/requestor-showcase/target/requestor-showcase-$1/WEB-INF $1/examples/showcase
+#mkdir $1/examples
+#cp -r ../requestor/examples/requestor-showcase/src/main/webapp $1/examples/showcase/
+#cp -r ../requestor/examples/requestor-showcase/target/requestor-showcase-$1/showcase $1/examples/showcase
+#cp -r ../requestor/examples/requestor-showcase/target/requestor-showcase-$1/WEB-INF $1/examples/showcase
 git add .
 git commit -m "Building site for Requestor $1"
 
