3df5511c72142176fca5aa8db07e37467aac995e
Danilo Reinert <daniloreinert@gmail.com>
2022-03-15 18:37:57 -0300

    Set MD5 DigestAuth on RequestorNet.init

--- requestor/impl/requestor-net/src/main/java/io/reinert/requestor/net/RequestorNet.java
+++ requestor/impl/requestor-net/src/main/java/io/reinert/requestor/net/RequestorNet.java
@@ -16,9 +16,12 @@
 package io.reinert.requestor.net;
 
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 import io.reinert.requestor.core.Requestor;
+import io.reinert.requestor.core.auth.DigestAuth;
 
 /**
  * This class provides a static initializer for Requestor's deferred bindings for JVM environment.
@@ -26,6 +29,8 @@ import io.reinert.requestor.core.Requestor;
  * @author Danilo Reinert
  */
 public class RequestorNet {
+
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
     /**
      * Initializes static lazy bindings to proper usage of Requestor in JVM environment.
@@ -40,5 +45,35 @@ public class RequestorNet {
                     new NetUriCodec()
             );
         }
+
+        if (!DigestAuth.hasHashFunction("md5")) {
+            try {
+                MessageDigest.getInstance("MD5");
+                DigestAuth.setHashFunction("md5", new DigestAuth.HashFunction() {
+                    public String hash(String input) {
+                        MessageDigest md = null;
+                        try {
+                            md = MessageDigest.getInstance("MD5");
+                        } catch (NoSuchAlgorithmException e) {
+                            throw new UnsupportedOperationException(
+                                    "Cannot perform MD5 hashing because the MD5 MessageDigest is not available.", e);
+                        }
+                        md.update(input.getBytes(StandardCharsets.UTF_8));
+                        byte[] digest = md.digest();
+                        return bytesToHex(digest);
+                    }
+                });
+            } catch (NoSuchAlgorithmException ignored) { }
+        }
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 }
