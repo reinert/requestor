0cbe6dc1dd6871f5861e0327d3ce0bbb01183180
Danilo Reinert <daniloreinert@gmail.com>
2022-03-16 15:37:29 -0300

    #84 [jn] Introduce ChunkedProgressEvent

---
+++ requestor/impl/requestor-net/src/main/java/io/reinert/requestor/net/ChunkedProgressEvent.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022 Danilo Reinert
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.reinert.requestor.net;
+
+import io.reinert.requestor.core.ProgressEvent;
+
+public class ChunkedProgressEvent implements ProgressEvent {
+
+    private static final long serialVersionUID = 8046158334061085390L;
+
+    private final int bytesLoaded;
+
+    public ChunkedProgressEvent(int bytesLoaded) {
+        this.bytesLoaded = bytesLoaded;
+    }
+
+    @Override
+    public boolean lengthComputable() {
+        return false;
+    }
+
+    @Override
+    public double loaded() {
+        return bytesLoaded;
+    }
+
+    @Override
+    public double total() {
+        return 0;
+    }
+}
