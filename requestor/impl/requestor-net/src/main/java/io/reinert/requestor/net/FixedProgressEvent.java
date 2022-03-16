7e61c8c023becdb3787529493a5c71bcd83aadc4
Danilo Reinert <daniloreinert@gmail.com>
2022-03-16 15:37:15 -0300

    #84 [jn] Introduce FixedProgressEvent

---
+++ requestor/impl/requestor-net/src/main/java/io/reinert/requestor/net/FixedProgressEvent.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022 Danilo Reinert
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.reinert.requestor.net;
+
+import io.reinert.requestor.core.ProgressEvent;
+
+public class FixedProgressEvent implements ProgressEvent {
+
+    private static final long serialVersionUID = -1457407228853688138L;
+
+    private final int bytesLoaded;
+    private final int bytesTotal;
+
+    public FixedProgressEvent(int bytesLoaded, int bytesTotal) {
+        this.bytesLoaded = bytesLoaded;
+        this.bytesTotal = bytesTotal;
+    }
+
+    @Override
+    public boolean lengthComputable() {
+        return true;
+    }
+
+    @Override
+    public double loaded() {
+        return bytesLoaded;
+    }
+
+    @Override
+    public double total() {
+        return bytesTotal;
+    }
+}
