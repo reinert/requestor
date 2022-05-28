d05bced7f9b4a8e1d52e0af56a252cce83856291
Danilo Reinert <daniloreinert@gmail.com>
2022-05-28 14:11:11 -0300

    #105 [api] Add getCharset to DeserializationContext

--- requestor/core/requestor-api/src/main/java/io/reinert/requestor/core/serialization/DeserializationContext.java
+++ requestor/core/requestor-api/src/main/java/io/reinert/requestor/core/serialization/DeserializationContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2021 Danilo Reinert
+ * Copyright 2014-2022 Danilo Reinert
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,5 +44,9 @@ public abstract class DeserializationContext {
 
     public Class<?> getParameterizedType() {
         return parameterizedType;
+    }
+
+    public String getCharset() {
+        return charset;
     }
 }
