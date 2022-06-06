23b8f41cf9d879d2d2a95f3d0d34611f6ba61cb6
Danilo Reinert <daniloreinert@gmail.com>
2022-06-06 00:18:13 -0300

    #112 [core] Introduce IncomingResponse interface

---
+++ requestor/core/requestor-core/src/main/java/io/reinert/requestor/core/IncomingResponse.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022 Danilo Reinert
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.reinert.requestor.core;
+
+import java.util.concurrent.Future;
+
+import io.reinert.requestor.core.payload.Payload;
+import io.reinert.requestor.core.payload.SerializedPayload;
+
+/**
+ * An HTTP response with futures.
+ *
+ * @author Danilo Reinert
+ */
+public interface IncomingResponse extends ResponseHeader {
+
+    /**
+     * Return the serialized payload future.
+     *
+     * @return the response's serialized payload future
+     */
+    Future<SerializedPayload> getSerializedPayload();
+
+    /**
+     * Return the response payload future.
+     *
+     * @return the response's payload future
+     */
+    Future<Payload> getPayload();
+
+}
