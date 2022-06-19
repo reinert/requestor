712d5260f5a48371ad159b99e84f50d40f29191a
Danilo Reinert <daniloreinert@gmail.com>
2022-06-19 10:20:29 -0300

    #125 [jnet] Add gzip write test

--- requestor/impl/requestor-javanet/src/test/java/io/reinert/requestor/java/net/GzipSerializationTest.java
+++ requestor/impl/requestor-javanet/src/test/java/io/reinert/requestor/java/net/GzipSerializationTest.java
@@ -15,7 +15,15 @@
  */
 package io.reinert.requestor.java.net;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Base64;
+import java.util.Random;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.zip.GZIPOutputStream;
 
 import io.reinert.requestor.core.Response;
 import io.reinert.requestor.core.Session;
@@ -65,6 +73,62 @@ public class GzipSerializationTest extends JavaNetTest {
                     Assert.assertTrue(body.contains("\"gzipped\": true"));
                     Assert.assertTrue(body.contains("\"Accept-Encoding\": \"gzip\""));
                     Assert.assertTrue(body.contains("\"Content-Encoding\": \"gzip\""));
+                }))
+                .onFail(failOnEvent(result))
+                .onError(failOnError(result));
+
+        finishTest(result, TIMEOUT);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testGzipInputStream() throws Throwable {
+        final TestResult result = new TestResult();
+
+        final Session session = Requestor.newSession(Executors.newSingleThreadScheduledExecutor());
+        session.save(Requestor.GZIP_ENCODING_ENABLED, Boolean.TRUE);
+
+        JavaNetRequestDispatcherFactory dispatcherFactory =
+                (JavaNetRequestDispatcherFactory) session.getRequestDispatcherFactory();
+
+        final byte[] bytes = new byte[(dispatcherFactory.getOutputBufferSize() * 2) + 1];
+        new Random().nextBytes(bytes);
+
+        final ByteArrayOutputStream bos = new ByteArrayOutputStream(bytes.length);
+        final GZIPOutputStream gzip = new GZIPOutputStream(bos);
+        gzip.write(bytes);
+        gzip.close();
+        final String expected = Base64.getEncoder().encodeToString(bos.toByteArray());
+        bos.close();
+
+        final InputStream inputStream = new ByteArrayInputStream(bytes);
+
+        final int expectedProgressCalls = 3;
+        final AtomicInteger progressCalls = new AtomicInteger(0);
+
+        final byte[][] buffers = new byte[expectedProgressCalls][];
+
+        final AtomicLong bytesWritten = new AtomicLong(0);
+
+        session.req("https://httpbin.org/post")
+                .payload(inputStream)
+                .save(Requestor.WRITE_CHUNKING_ENABLED, true)
+                .post(String.class)
+                .onWrite(p -> buffers[progressCalls.get()] = p.getChunk().asBytes())
+                .onWrite(p -> bytesWritten.set(p.getLoaded()))
+                .onWrite(p -> progressCalls.addAndGet(1))
+                .onSuccess(test(result, (String body) -> {
+                    String content = body.substring(body.indexOf("\"data\"") + 46, body.indexOf("\"files\"") - 6);
+                    Assert.assertEquals(expected, content);
+
+                    Assert.assertEquals(expectedProgressCalls, progressCalls.get());
+                    Assert.assertEquals(bytes.length, bytesWritten.get());
+
+                    byte[] joinedBuffers = new byte[bytes.length];
+                    for (int i = 0, k = 0; i < expectedProgressCalls; i++) {
+                        System.arraycopy(buffers[i], 0, joinedBuffers, k, buffers[i].length);
+                        k += buffers[i].length;
+                    }
+                    Assert.assertArrayEquals(bytes, joinedBuffers);
                 }))
                 .onFail(failOnEvent(result))
                 .onError(failOnError(result));
