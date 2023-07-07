ea1433d6e5dd50f3ae7041247b1eea9f7987ec4e
Danilo Reinert <daniloreinert@gmail.com>
2023-07-07 15:07:34 -0300

    #157 [jnet] Introduce CertAuth

---
+++ requestor/impl/requestor-javanet/src/main/java/io/reinert/requestor/java/net/auth/CertAuth.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 Danilo Reinert
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.reinert.requestor.java.net.auth;
+
+import java.net.HttpURLConnection;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+
+import io.reinert.requestor.core.Auth;
+import io.reinert.requestor.core.PreparedRequest;
+import io.reinert.requestor.java.net.JavaNetHttpConnection;
+
+/**
+ * Client Certificate Authentication based on {@link SSLContext}.
+ *
+ * @author Danilo Reinert
+ */
+public class CertAuth implements Auth {
+
+    private final SSLContext sslContext;
+
+    public CertAuth(SSLContext sslContext) {
+        this.sslContext = sslContext;
+    }
+
+    @Override
+    public void auth(PreparedRequest request) {
+        request.setConnectionPreparer(connection -> {
+            JavaNetHttpConnection netConn = (JavaNetHttpConnection) connection;
+            HttpURLConnection httpConn = netConn.getHttpUrlConnection();
+            if (httpConn instanceof HttpsURLConnection) {
+                ((HttpsURLConnection) httpConn).setSSLSocketFactory(this.sslContext.getSocketFactory());
+            }
+        });
+
+        request.send();
+    }
+}
